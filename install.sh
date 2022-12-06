332723b84c7e4e093aeec76034352a1313c5f3a4
Salvador Gudino <cero@duck.com>
2022-12-05 23:30:44 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This is script MUST be run by root/sudo
+if [ "$(id -u)" != "0" ]; then
+    echo "Error: This script MUST be run by root/sudo"
+    exit 1
+fi
+
+SCRIPT_PATH=$(dirname "$(realpath "${BASH_SOURCE[0]-$0}")")
+
+# Using + as separator instead of /
+sed "s+ExecStart\=+ExecStart\=${SCRIPT_PATH}/play.py+g" "${SCRIPT_PATH}"/parental-limit.service
+sudo cp "${SCRIPT_PATH}"/parental-limit.service /etc/systemd/user
+sudo systemctl enable --now parental-limit.service
